0545ba328e6f0267b5843f131b5414cc10c56179
unknown <Administrator@Asiet-Lab.ASIETDomain.com>
2019-01-17 16:30:52 +0530

    java

---
+++ javascri/Java.js
@@ -0,0 +1,5 @@
+var j="jothish"
+console.log(j)
+const v=50;
+a=v+10;
+console.log(a)
---
+++ javascri/even.js
@@ -0,0 +1,9 @@
+var x=10;
+if(x%2==0)
+    {
+        console.log("The given no is even")
+    }
+else
+    {
+        console.log("the give no is odd :D")
+    }
